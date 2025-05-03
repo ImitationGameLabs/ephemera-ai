80b326cf781081a15340977e99ea558545f398f7
Simplex <fochive00@gmail.com>
2025-05-04 00:02:42 +0800

    refactor(ai): simplify prompt loop

--- ephemera-ai/src/ephemera/ephemera.rs
+++ ephemera-ai/src/ephemera/ephemera.rs
@@ -1,9 +1,12 @@
 use rig::{
     agent::Agent,
-    completion::{CompletionModel, Completion, PromptError},
+    completion::{Completion, CompletionModel, PromptError},
     message::{AssistantContent, Message, ToolCall, ToolFunction, ToolResultContent, UserContent},
-    OneOrMany,
+    tool::ToolSetError,
+    OneOrMany
 };
+
+use tracing::debug;
 
 pub struct Ephemera<M: CompletionModel> {
     pub agent: Agent<M>,
@@ -17,60 +20,58 @@ impl<M: CompletionModel> Ephemera<M> {
     ) -> Result<String, PromptError> {
         let mut current_prompt: Message = prompt.into();
         loop {
-            println!("Current Prompt: {:?}\n", current_prompt);
-            let resp = self
+            debug!("Current Prompt: {:?}\n", current_prompt);
+            let resp: rig::completion::CompletionResponse<<M as CompletionModel>::Response> = self
                 .agent
                 .completion(current_prompt.clone(), self.chat_history.clone())
                 .await?
                 .send()
                 .await?;
 
-            let mut final_text = None;
+            self.chat_history.push(current_prompt.clone());
 
-            for content in resp.choice.into_iter() {
-                match content {
-                    AssistantContent::Text(text) => {
-                        println!("Intermediate Response: {:?}\n", text.text);
-                        final_text = Some(text.text.clone());
-                        self.chat_history.push(current_prompt.clone());
-                        let response_message = Message::Assistant {
-                            content: OneOrMany::one(AssistantContent::text(&text.text)),
-                        };
-                        self.chat_history.push(response_message);
-                    }
-                    AssistantContent::ToolCall(content) => {
-                        self.chat_history.push(current_prompt.clone());
-                        let tool_call_msg = AssistantContent::ToolCall(content.clone());
-                        println!("Tool Call Msg: {:?}\n", tool_call_msg);
+            // We only process the first choice.
+            let content = resp.choice.first();
+            self.chat_history.push(Message::Assistant { 
+                content: OneOrMany::one(content.clone()) 
+            });
 
-                        self.chat_history.push(Message::Assistant {
-                            content: OneOrMany::one(tool_call_msg),
-                        });
+            match content {
+                AssistantContent::Text(text) => {
+                    debug!("Intermediate Response: {:?}\n", text.text);
+                    return Ok(text.text);
+                }
+                AssistantContent::ToolCall(content) => {
+                    let tool_result =  self.tool_call(content).await?;
 
-                        let ToolCall {
-                            id,
-                            function: ToolFunction { name, arguments },
-                        } = content;
-
-                        let tool_result =
-                            self.agent.tools.call(&name, arguments.to_string()).await?;
-
-                        current_prompt = Message::User {
-                            content: OneOrMany::one(UserContent::tool_result(
-                                id,
-                                OneOrMany::one(ToolResultContent::text(tool_result)),
-                            )),
-                        };
-
-                        final_text = None;
-                        break;
-                    }
+                    current_prompt = Message::User {
+                        content: OneOrMany::one(tool_result),
+                    };
                 }
             }
-
-            if let Some(text) = final_text {
-                return Ok(text);
-            }
         }
+    }
+
+    async fn tool_call(&mut self, content: ToolCall) -> Result<UserContent, ToolSetError> {
+        let tool_call_msg = AssistantContent::ToolCall(content.clone());
+        debug!("Tool Call Msg: {:?}\n", tool_call_msg);
+
+        self.chat_history.push(Message::Assistant {
+            content: OneOrMany::one(tool_call_msg),
+        });
+
+        let ToolCall {
+            id,
+            function: ToolFunction { name, arguments },
+        } = content;
+
+        let tool_result = self.agent.tools
+            .call(&name, arguments.to_string())
+            .await?;
+
+        Ok(UserContent::tool_result(
+            id,
+            OneOrMany::one(ToolResultContent::text(tool_result)),
+        ))
     }
 }
